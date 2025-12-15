692d6ff2eab0d233eed6a0b4bc9364430ea93c75
eddiedubica <eddiedubica@gmail.com>
2025-12-15 22:25:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
-const axios = require('axios');
-const crypto = require('crypto');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
